db8b5f299e4f4beef652c680730157ebb2e52783
Roman Lamsal <roman@lamsal.de>
2020-12-21 16:00:21 +0100

    initial commit

---
+++ compare_digests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 2020-12-21: DOCKER_CLI_EXPERIMENTAL=enabled must be set in order for this to work
+# Returns 0 when there are no changes and 1 when there are changes
+SOURCE_IMAGE="$1"
+TARGET_IMAGE="$2"
+
+if docker manifest inspect "$SOURCE_IMAGE" > sourcemanifest.json; then
+  SOURCE_IMAGE_DIGEST=$(jq --raw-output '.config.digest' < ./sourcemanifest.json)
+  if docker manifest inspect "$TARGET_IMAGE" > targetmanifest.json; then
+    TARGET_IMAGE_DIGEST=$(jq --raw-output '.config.digest' < ./targetmanifest.json)
+    if [ "$SOURCE_IMAGE_DIGEST" = "$TARGET_IMAGE_DIGEST" ]; then
+      exit 0
+    fi
+  fi
+  exit 2
+fi
+
+exit 1
